9550ce9ec019e50c6e02563e30427d2abd2e259c
Long Sun <spritesun@gmail.com>
2015-03-25 16:45:58 +1100

    Init repo

---
+++ QuestionsApp/QuestionsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuestionsApp
+//
+//  Created by Long Sun on 25/03/2015.
+//  Copyright (c) 2015 Sunlong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
